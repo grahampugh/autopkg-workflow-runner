1c45be386069381089a574ffe2ded9d36f1ff357
Graham Pugh <5802725+grahampugh@users.noreply.github.com>
2026-02-26 20:49:44 +0000

    add command

--- jamf-recipe-run.sh
+++ jamf-recipe-run.sh
@@ -16,7 +16,17 @@ autopkg_run() {
     local RECIPE="$1"
     shift
 
-    ./../autopkg/.venv/bin/python3 ../autopkg/Code/autopkg run "$verbosity" --prefs "$AUTOPKG_PREFS" "$RECIPE" "$@"
+    autopkg_cmd run "$verbosity" --prefs "$AUTOPKG_PREFS" "$RECIPE" "$@"
+}
+
+autopkg_cmd() {
+    # Run autopkg with any extra parameters
+    # check if the platform is ARM Mac or Ubuntu
+    if [[ "$(uname)" == "Darwin" ]]; then
+        autopkg "$@"
+    else
+        ./../autopkg/.venv/bin/python3 ../autopkg/Code/autopkg "$@"
+    fi
 }
 
 ## MAIN
@@ -28,45 +38,45 @@ verbosity="-v"
 inputted_jamf_recipes=()
 extra_key=()
 
-while test $# -gt 0
-do
+while test $# -gt 0; do
     case "$1" in
-        -h|--help)
-            usage
-            ;;
-        --prefs)
-            # prefs plist can be supplied. Defaults to "$HOME/Library/Preferences/com.github.autopkg.plist"
-            shift
-            AUTOPKG_PREFS="$1"
-            [[ $AUTOPKG_PREFS == "/"* ]] || AUTOPKG_PREFS="$(pwd)/${AUTOPKG_PREFS}"
-            ;;
-        -r|--recipe)
-            shift
-            echo "Recipe: $1"
-            inputted_jamf_recipes+=("$1")
-            ;;
-        -l|--recipe-list)
-            shift
-            echo "Recipe list: $1"
-            AUTOPKG_RECIPE_LIST="$1"
-            ;;
-        --key)
-            shift
-            echo "Extra key: $1"
-            extra_key+=(
-                "--key"
-                "$1"
-            )
-            ;;
-        -v*) verbosity=$1
-            ;;
-        --replace)
-            replace_pkg=1
-            ;;
-        *)
-            echo "Unknown parameter: $1"
-            usage
-            ;;
+    -h | --help)
+        usage
+        ;;
+    --prefs)
+        # prefs plist can be supplied. Defaults to "$HOME/Library/Preferences/com.github.autopkg.plist"
+        shift
+        AUTOPKG_PREFS="$1"
+        [[ $AUTOPKG_PREFS == "/"* ]] || AUTOPKG_PREFS="$(pwd)/${AUTOPKG_PREFS}"
+        ;;
+    -r | --recipe)
+        shift
+        echo "Recipe: $1"
+        inputted_jamf_recipes+=("$1")
+        ;;
+    -l | --recipe-list)
+        shift
+        echo "Recipe list: $1"
+        AUTOPKG_RECIPE_LIST="$1"
+        ;;
+    --key)
+        shift
+        echo "Extra key: $1"
+        extra_key+=(
+            "--key"
+            "$1"
+        )
+        ;;
+    -v*)
+        verbosity=$1
+        ;;
+    --replace)
+        replace_pkg=1
+        ;;
+    *)
+        echo "Unknown parameter: $1"
+        usage
+        ;;
     esac
     shift
 done
@@ -78,9 +88,9 @@ echo "AutoPkg prefs file: $AUTOPKG_PREFS"
 if [[ -f "$AUTOPKG_RECIPE_LIST" ]]; then
     # create recipe list from file
     inputted_recipe_list=()
-    while IFS= read -r; do 
+    while IFS= read -r; do
         inputted_recipe_list+=("$REPLY")
-    done < "$AUTOPKG_RECIPE_LIST" 
+    done <"$AUTOPKG_RECIPE_LIST"
 elif [[ "${#inputted_jamf_recipes[@]}" -gt 0 ]]; then
     # get list from command line
     inputted_recipe_list=("${inputted_jamf_recipes[@]}")
@@ -110,7 +120,7 @@ for inputted_jamf_recipe in "${inputted_recipe_list[@]}"; do
     extra_options=("${extra_key[@]}")
 
     echo "Running '$inputted_jamf_recipe' on '$JSS_URL'"
-    if ! autopkg_run "$inputted_jamf_recipe" "${extra_options[@]}" ; then
+    if ! autopkg_run "$inputted_jamf_recipe" "${extra_options[@]}"; then
         echo "Recipe failed."
     fi
 done
