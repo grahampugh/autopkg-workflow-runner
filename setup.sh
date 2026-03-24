72f75ae44097d69e52034be6a29874fbe5193499
Graham Pugh <5802725+grahampugh@users.noreply.github.com>
2026-03-24 17:05:19 +0100

    use uv

--- setup.sh
+++ setup.sh
@@ -10,47 +10,35 @@
 setup_ubuntu() {
     echo "Running on Linux - performing Ubuntu setup"
 
+    uv python install 3.10
+
     sudo apt update && DEBIAN_FRONTEND=noninteractive apt install -y git
-    # git clone https://github.com/jgstew/jgstew-recipes.git
-
-    # setup python3.10: https://gist.github.com/rutcreate/c0041e842f858ceb455b748809763ddb
-    sudo DEBIAN_FRONTEND=noninteractive apt install -y software-properties-common git
-    sudo add-apt-repository ppa:deadsnakes/ppa -y && apt update
-
-    sudo DEBIAN_FRONTEND=noninteractive apt install -y python3.10 python3.10-venv python3.10-dev
-
-    # https://pip.pypa.io/en/stable/installation/#ensurepip
-    sudo python3.10 -m ensurepip --upgrade
-
-    # update python pip
-    sudo python3.10 -m pip install --upgrade pip
-
-    # update python basics
-    sudo python3.10 -m pip install --upgrade setuptools wheel build Foundation
 
     # install packages needed for installing python requirements and using python processors
-    sudo DEBIAN_FRONTEND=noninteractive apt install -y python-dev-is-python3 speech-dispatcher libcairo2-dev libmagic-dev jq p7zip-full msitools curl git wget build-essential libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+    sudo DEBIAN_FRONTEND=noninteractive apt install -y speech-dispatcher libcairo2-dev libmagic-dev jq p7zip-full msitools curl wget
 
     # if autopkg does not exist
     if [ ! -f ../autopkg ]; then
         git clone https://github.com/autopkg/autopkg.git ../autopkg
-    # bash -c "cd ../autopkg && git checkout dev"
     fi
 
-    # create virtual environment
-    python3.10 -m venv ../autopkg/.venv
-    ./../autopkg/.venv/bin/python3 -m pip install --upgrade pip
-    ./../autopkg/.venv/bin/python3 -m pip install --upgrade setuptools wheel build
+    # create virtual environment using uv
+    uv venv --python 3.10 ../autopkg/.venv
 
-    # install autopkg requirements
-    ./../autopkg/.venv/bin/python3 -m pip install --requirement ../autopkg/gh_actions_requirements.txt
+    # install minimal autopkg runtime dependencies
+    uv pip install --python ../autopkg/.venv/bin/python3 \
+        pyyaml certifi xattr appdirs
 
-    # install jgstew-recipes requirements:
-    ./../autopkg/.venv/bin/python3 -m pip install --requirement requirements.txt
+    # install jgstew-recipes requirements (if requirements.txt exists):
+    if [[ -f requirements.txt ]]; then
+        uv pip install --python ../autopkg/.venv/bin/python3 \
+            --requirement requirements.txt
+    fi
 
     # fix issue with new openssl and a processor
     # https://github.com/wbond/oscrypto/issues/78#issuecomment-2210120532
-    ./../autopkg/.venv/bin/python3 -m pip install -I git+https://github.com/wbond/oscrypto.git
+    uv pip install --python ../autopkg/.venv/bin/python3 \
+        git+https://github.com/wbond/oscrypto.git
 
 }
 
